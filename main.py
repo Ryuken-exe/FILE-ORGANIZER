96a24ee2a1fe97354d5d834ce2a89bab7e1d3bc2
Ryuken <Shivsonar73@gmail.com>
2026-09-12 19:46:08 +0530

    File Organizer — 7 Steps, 2st Step Clear

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #BUILDING FILE ORGANIZER
 import os 
-path = input("ENTER YOUR PATH > ").strip()
+from pathlib import Path
+path = "C:\\Users\\Shiva\\Downloads\\newfiles"
 if os.path.exists(path):
-	folders = os.listdir(path)
-	for file in folders:
-		print(file)
+	files = os.listdir(path)
+	for file in files:	
+		full_path = os.path.join(path,file)
+		if os.path.isfile(full_path):
+			print(full_path)
+
 else:
 	print("Sorry Invalid Path")

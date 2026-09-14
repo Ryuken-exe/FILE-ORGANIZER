db1217e408eb84aef25199fb8754f0b9c4037ec9
Ryuken <Shivsonar73@gmail.com>
2026-09-14 19:27:42 +0530

    File Organizer — 7 Steps, 5st Step Clear.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 #BUILDING FILE ORGANIZER
 import os 
 from pathlib import Path
+import shutil
+managing = {
+	"Images" : [".jpg", ".jpeg", ".png", ".gif"],
+	"Videos" : [".mp4", ".mkv", ".avi"],
+	"PDFs"   : [".pdf"],
+	"Music"  : [".mp3", ".wav"]
+}
 path = "C:\\Users\\Shiva\\Downloads\\newfiles"
 if os.path.exists(path):
 	files = os.listdir(path)
 	for file in files:	
-		full_path = os.path.join(path,file)
+		full_path = os.path.join(path,file) #new path resource 
 		if os.path.isfile(full_path):
 			print(full_path)
 			full_path = Path(full_path)
 			extension = full_path.suffix
-			print(extension)
+			for category,extensions in managing.items():
+				if extension in extensions:
+					new_files = os.path.join(path,category)
+					os.makedirs(new_files,exist_ok=True)
+					pass
+				
+
 	
 
 else:
